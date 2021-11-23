dac994b99f0caa4c57c551685fe7b74aae56f638
Dev Agrawal <devagrawal09@gmail.com>
2021-11-23 00:21:13 -0500

    added mentorship expeirence showcase

--- src/pages/experiences/index.js
+++ src/pages/experiences/index.js
@@ -11,6 +11,38 @@ const ExperiencesPage = ({ location }) => (
         <h1 className="wheat-text">My Honors Experiences</h1>
         <div className="product-item">
           <Link
+            to="mentorship"
+            style={{ textDecoration: `none`, color: `black` }}
+            state={{ title: `UHP Welcome Experience Mentorship` }}
+          >
+            <div className="product-item-title d-flex">
+              <div className="bg-faded p-5 d-flex ml-auto rounded">
+                <h2 className="section-heading mb-0">
+                  <span className="section-heading-upper">August 2021</span>
+                  <span className="section-heading-lower">
+                    UHP Welcome Experience Mentorship
+                  </span>
+                </h2>
+              </div>
+            </div>
+            <div className="showcase">
+              <img
+                className="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+                src={revucLogo}
+                alt=""
+              />
+            </div>
+            <div className="product-item-description d-flex mr-auto">
+              <div className="bg-faded p-5 rounded">
+                <p className="mb-0">
+                  Mentoring the next generation of honors
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="product-item">
+          <Link
             to="revolutionuc"
             style={{ textDecoration: `none`, color: `black` }}
             state={{ title: `RevolutionUC Organizing` }}
